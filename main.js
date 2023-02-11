c14c5eed0eb4e1f8bb675b9bb01fe5d6dddf8dd1
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2023-02-11 22:19:03 +0300

    add slip id

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ fetch('https://api.adviceslip.com/advice')
             throw new Error('Something went wrong');
         }})
     .then(data => {
-        console.log(data.slip.advice)
+        console.log(data.slip)
+        document.getElementById('advice-id').innerHTML = data.slip.id;
         document.getElementById('advice-text').innerHTML = data.slip.advice;
         }
     )
